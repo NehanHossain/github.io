ecbefb1839c2cab54a7f130ffbdedd905653d662
NehanHossain <156833031+NehanHossain@users.noreply.github.com>
2025-01-17 00:10:20 -0500

    commit

--- scripts/app.js
+++ scripts/app.js
@@ -12,6 +12,7 @@
             location.href = "about.html";
         });
 
+<<<<<<< HEAD
         let MainContent = document.getElementsByName("MainContent")[0];
 
         //<p id="MainParagraph" class="mt-3">this is my first paragraph
@@ -36,6 +37,36 @@
         Article.setAttribute("class", "container");
         Article.innerHTML = ArticleParagraph;
         DocumentBody.appendChild(Article);
+=======
+        let MainContent = document.getElementsByTagName("main")[0];
+        //<p id="MainParagraph" class="mt-3">This is my first paragraph</p>
+        let MainParagraph = document.createElement("p");
+        MainParagraph.setAttribute("id", "MainParagraph");
+        MainParagraph.setAttribute("class", "mt-3");
+        MainParagraph.textContent = "This is my first paragraph";
+
+        //Attach to the dom
+        MainContent.appendChild(MainParagraph);
+
+        let FirstString = "This is";
+        let SecondString = `${FirstString} is my second paragraph`;
+        MainParagraph.textContent = SecondString;
+
+        //Attach to the dom
+        MainContent.appendChild(MainParagraph);
+
+        let DocumentBody = document.body;
+        //<article><p></p></article>
+        let Article = document.createElement("article");
+        let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is my article paragraph</p>`;
+        Article.setAttribute("class", "container");
+        Article.innerHTML = ArticleParagraph;
+        DocumentBody.appendChild(Article);
+
+
+
+
+>>>>>>> 0c3abb2 (commit)
     }
 
     function DisplayAboutPage(){
